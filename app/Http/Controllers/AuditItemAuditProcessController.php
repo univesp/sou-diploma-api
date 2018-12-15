ed4a607c22b929b24786b6a59fe7c992ae249875
eduardo oliveira <eduardooliveira@eduardos-MacBook-Pro.local>
2018-12-15 09:53:16 -0200

    Adds route and modify the names in controllers.

--- app/Http/Controllers/AuditItemAuditProcessController.php
+++ app/Http/Controllers/AuditItemAuditProcessController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AuditItemAuditProcesseController extends Controller
+class AuditItemAuditProcessController extends Controller
 {
     /**
      * Display a listing of the resource.
