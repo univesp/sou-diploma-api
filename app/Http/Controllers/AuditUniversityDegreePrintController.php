77912f723b7f9912bb354fad758888f6c9edfe65
Eduardo Oliveira <eduardo.oliveira@connectcom.univesp.br>
2018-12-21 12:06:12 -0200

    Removed controller and modify pdf

--- app/Http/Controllers/AuditUniversityDegreePrintController.php
+++ app/Http/Controllers/AuditUniversityDegreePrintController.php
@@ -69,14 +69,85 @@ class AuditUniversityDegreePrintController extends Controller
 
         $student_id = 65537;
 
-        //$data = DB::connection('mysql_sa')->table('v_impressao_diploma')->where('student_id',$student_id)->get();
+        $universityDegree = DB::connection('mysql_sa')->table('v_impressao_diploma')->where('student_id',$student_id)->first();
+        //numero_rotulo": "Undergraduation;bacharel;Licenciatura em Biologia;2;2018;1° via"
+        $rotulo = explode(';',$universityDegree->numero_rotulo);
         
-        //dd($data);
+        if($rotulo[0] == 'Undergraduation') {
+            $rotulo[0] = 'GR';
+        }
 
-        $universityDegree = [];
-        
+        if($rotulo[1] == 'bacharel') {
+            $rotulo[1] = 'B';
+        }
+
+        $rotulo[2] = 'LB';
+        $rotulo[3] = '00002';
+        $sequencial = $rotulo[3];
+
+        $rotulo = "{$rotulo[0]}.{$rotulo[1]}.{$rotulo[2]}.{$rotulo[3]}.{$rotulo[4]}.{$rotulo[5]}.{$rotulo[3]}"; 
+
+        $birth = explode("/",$universityDegree->birth_date);
+        $conclusion = explode("/",$universityDegree->date_conclusion);
+
+        $universityDegree->conclusion_mouth = $this->dayOrMouth($conclusion[1]);
+        $universityDegree->conclusion_year  = $conclusion[2]; 
+
+        $universityDegree->day = $birth[0];
+        $universityDegree->mouth = $this->dayOrMouth($birth[1]);;
+        $universityDegree->year = $birth[2];
+
+        $universityDegree->numero_rotulo = $rotulo;
+
+        //dd($universityDegree);
+
         $pdf = PDF::loadView('pdf.universityDegree', ['universityDegree' => $universityDegree])->setPaper('a4', 'landscape');
 
         return $pdf->download('Diploma - ' . Carbon::now()->format('H:i:s d-m-Y') . '.pdf');
+    }
+
+    private function dayOrMouth($day)
+    {
+        $mouth ='';
+        switch($day) {
+            case '01':
+                $mouth = 'Janeiro';
+            break;
+            case '02':
+                $mouth = 'Fevereiro';
+            break;
+            case '03':
+                $mouth = 'Março';
+            break;
+            case '04':
+                $mouth = 'Abril';
+            break;
+            case '05':
+                $mouth = 'Maio';
+            break;
+            case '06':
+                $mouth = 'Junho';
+            break;
+            case '07':
+                $mouth = 'Julho';
+            break;
+            case '08':
+                $mouth = 'Agosto';
+            break;
+            case '09':
+                $mouth = 'Setembro';
+            break;
+            case '10':
+                $mouth = 'Outubro';
+            break;
+            case '11':
+                $mouth = 'Novembro';
+            break;
+            case '12':
+                $mouth = 'Dezembro';
+            break;
+        }
+
+        return $mouth;
     }
 }
