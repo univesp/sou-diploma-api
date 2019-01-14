43df2972cf37c50a8e5f120c57546e8f1804e6d8
rubenssamara <rubens.samara@connectcom.univesp.br>
2019-01-14 15:39:13 -0200

    Receiving array data from sou-diploma-ui in PrintListTempController

--- app/Http/Controllers/PrintListTempController.php
+++ app/Http/Controllers/PrintListTempController.php
@@ -2,21 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use App\PrintListTemp;
+use App\Models\PrintListTemp;
+use http\Env\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PrintListTempController extends Controller
 {
     public function printStatus(Request $request)
     {
-        var_dump($request);
+//        var_dump($request);
 
-        $printStatus = PrintListTemp::find($request->id);
-        $printStatus->status_impress = 1;
+//        $printStatus = PrintListTemp::where('ra', '=', $request->student_id)->first();
+//        $printStatus->status_impress = 1;
+//        $printStatus->save();
+
+        $printStatus = DB::table('print_list_temp')->whereIn('RA', $request->ras)->get();
+
+        foreach ($printStatus as $print){
+            $print->status_impress = 1;
+        }
+
         $printStatus->save();
 
-        $printStatus = PrintListTemp::where('student_id', $request->student_id);
-        $printStatus->status_impress = 1;
-        $printStatus->save();
+//        if ($printStatus) {
+//            return response()->json([
+//                'Message' => 'Impressão comcluída com sucesso!'
+//            ]);
+//        }else{
+//            return response()->json([
+//                'Message' => 'Falha na impressão!'
+//            ]);
+//        }
     }
 }
